9254edd70cfb299e8d49d96aa2fe0223a90020c5
tedro <tedonmain@gmail.com>
2022-07-06 05:22:46 +0000

    JSX React Babelrc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const hello = () => {
+/*const hello = () => {
     console.log("hello world!");
     }
-    hello();
-    
+    hello();*/
+ 
+    import React from "react";
+    import ReactDOM from "react-dom";
+    class App extends React.Component {
+    render() {
+        return <div>Hello {this.props.name}</div>;
+        }
+    }
+    var mountNode = document.getElementById("app");
+    ReactDOM.render(<App name="Jane" />, mountNode);
