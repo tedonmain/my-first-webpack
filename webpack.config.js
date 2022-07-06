263bba036a8d5a66702303d03bdeadb29cd89f6b
tedro <tedonmain@gmail.com>
2022-07-06 05:57:35 +0000

    add jsx to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.(js)$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
                 }
