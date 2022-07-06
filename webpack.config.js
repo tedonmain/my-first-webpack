5c4e4db22c80167d8501715e76324428b4a86c50
tedro <tedonmain@gmail.com>
2022-07-06 04:20:39 +0000

    webpack module rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const webpack = require('webpack');
 const path = require('path');
 const config = {
-entry: './src/index.js',
-output: {
-path: path.resolve(__dirname, 'dist'),
-filename: 'bundle.js'
-}
+    entry: './src/index.js',
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+                }
+        ]
+    }
 }
 module.exports = config;
